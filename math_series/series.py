da26a750f5937c2eb0c1aeee4e250fa2a3dedb91
monaSalih <monaalobisat@gmail.com>
2021-10-11 22:23:46 +0300

    newFibo4

--- math_series/series.py
+++ math_series/series.py
@@ -4,5 +4,10 @@ def fibonacci(x):
     elif x==0:
         return 0
     else:
-        return(fibonacci(x-1)+fibonacci(x+2))
-        
+        return(fibonacci(x-1)+fibonacci(x-2))
+
+def lucas(x):
+    if(x==0):
+        return 2
+    else:
+        return (lucas(x-1)+lucas(x-2))

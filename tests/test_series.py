f9f5b68f77e56859b82bc8e293b1fd57f907fe82
monaSalih <monaalobisat@gmail.com>
2021-10-11 22:51:23 +0300

    newFibo11

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,6 +1,7 @@
 from math_series import __version__
 from math_series.series import fibonacci
 from math_series.series import lucas
+from math_series.series import sum_series
 
 
 def test_version():

db1977d13e666fc57cab94703be46a809773c651
monaSalih <monaalobisat@gmail.com>
2021-10-11 22:01:40 +0300

    fibTest2

--- math_series/series.py
+++ math_series/series.py
@@ -1,2 +1,8 @@
 def fibonacci(x):
-    return 8
+    if x==1:
+        return 1
+    if x==0:
+        return 0
+    else:
+        return(fibonacci(x-1)+fibonacci(x+2))
+        
--- tests/test_series.py
+++ tests/test_series.py
@@ -5,7 +5,14 @@ from math_series.series import fibonacci
 def test_version():
     assert __version__ == '0.1.0'
 
-def test_fibonacci():
+def test_fibonacci1():
+    # input
+    v=0
+    #output
+    fibonResult=1
+    assert fibonacci(v)==fibonResult
+
+def test_fibonacci2():
     # input
     v=5
     #output

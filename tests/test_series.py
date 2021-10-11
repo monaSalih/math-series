6a7d65b3e361f54d878cc07dc628459b04f5758f
monaSalih <monaalobisat@gmail.com>
2021-10-11 23:06:00 +0300

    after fix terminal4

--- tests/test_series.py
+++ tests/test_series.py
@@ -11,7 +11,7 @@ def test_fibonacci1():
     # input
     v=0
     #output
-    fibonResult=1
+    fibonResult=0
     assert fibonacci(v)==fibonResult
 
 def test_fibonacci2():
@@ -22,34 +22,34 @@ def test_fibonacci2():
 
 def test_fibonacci3():
     # input
-    v=0
+    v=3
     #output
     fibonResult=2
     assert fibonacci(v)==fibonResult
 
-# def lucas_test1():
-#     # input
-#     v=3
-#     #output
-#     lucasResult=0
-#     assert lucas(v)==lucasResult
+def lucas_test1():
+    # input
+    v=3
+    #output
+    lucasResult=0
+    assert lucas(v)==lucasResult
 
-# def lucas_test2():
-#     # input
-#     v=5
-#     #output
-#     lucasResult=11
-#     assert lucas(v)==lucasResult
+def lucas_test2():
+    # input
+    v=5
+    #output
+    lucasResult=11
+    assert lucas(v)==lucasResult
 
-# def lucas_test3():
-#     # input
-#     v=0
-#     #outputu
-#     lucasResult=2
-#     assert lucas(v)==lucasResult
+def lucas_test3():
+    # input
+    v=0
+    #outputu
+    lucasResult=2
+    assert lucas(v)==lucasResult
 
-# def test_sum_series():
-#     assert sum_series(3, 4, 2) == 8
+def test_sum_series():
+    assert sum_series(3, 4, 2) == 8
 
-# def test_sum_series():
-#     assert sum_series(5,5,5) == 40
+def test_sum_series2():
+    assert sum_series(5,5,5) == 40

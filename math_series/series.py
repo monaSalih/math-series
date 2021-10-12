9825212d95ff1afeee651c281995d5b9cc18de37
monaSalih <monaalobisat@gmail.com>
2021-10-12 16:13:27 +0300

    finaly done

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,10 @@
 def fibonacci(x):
+    """
+fibonaci series = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55…
+what value of 5 in fibonaci series mean the value for postion 5 in series
+input =3
+output =1
+    """
     if x==1:
         return 1
     elif x==0:
@@ -7,6 +13,12 @@ def fibonacci(x):
         return(fibonacci(x-1)+fibonacci(x-2))
 
 def lucas(x):
+    """
+licas series = 2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123 …………..
+what value of 5 in licas series mean the value for postion 5 in series
+input =5
+output =11
+    """
     if x==0:
         return 2
     elif x==1:
@@ -14,12 +26,33 @@ def lucas(x):
     else:
         return (lucas(x-1)+lucas(x-2))
 
-def sum_series(x,val1,val2):
+
+def mySeries(x,val1,val2):
+    
     if x==0:
         return val1
-    if x==1:
+    elif x==1:
         return val2
-    return (sum_series(x-1)+sum_series(x-2))
+    else:
+        return (mySeries(x-1,val1,val2)+mySeries(x-2,val1,val2))
+"""
+mySeries accept3 parameters 2 of them optional and give them(0,1) if they didnt enter
+if val1==0 and val2==1 go to fibonacci(x)
+if val1==2 and val2==1 go to return lucas(x)
+else calculate the sumation for input value
+ input=(5,5,5)
+ output= 40
+
+    """
+def sum_series(x,val1=0,val2=1):
+    if val1==0 and val2==1:
+        return fibonacci(x)
+    elif val1==2 and val2==1:
+        return lucas(x)
+    else:
+        return mySeries(x,val1,val2)
+
+
 
 
 
--- tests/test_series.py
+++ tests/test_series.py
@@ -6,50 +6,50 @@ from math_series.series import sum_series
 
 def test_version():
     assert __version__ == '0.1.0'
-
+#1
 def test_fibonacci1():
     # input
     v=0
     #output
     fibonResult=0
     assert fibonacci(v)==fibonResult
-
+#2
 def test_fibonacci2():
     # input
     v=2
     #output
     assert fibonacci(v)==1
-
+#3
 def test_fibonacci3():
     # input
     v=3
     #output
     fibonResult=2
     assert fibonacci(v)==fibonResult
-
-def lucas_test1():
+#4
+def test_lucas1():
     # input
     v=3
     #output
-    lucasResult=0
+    lucasResult=4
     assert lucas(v)==lucasResult
-
-def lucas_test2():
+#5
+def test_lucas2():
     # input
     v=5
     #output
     lucasResult=11
     assert lucas(v)==lucasResult
-
-def lucas_test3():
+#6
+def test_lucas3():
     # input
     v=0
     #outputu
     lucasResult=2
     assert lucas(v)==lucasResult
-
+#7
 def test_sum_series():
     assert sum_series(3, 4, 2) == 8
-
+#8
 def test_sum_series2():
     assert sum_series(5,5,5) == 40
